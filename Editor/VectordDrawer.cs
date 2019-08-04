da15cabcdf70e987c644738fd301c9cce4f1ec2c
TheWhiteShadow <tws@3freaks.de>
2019-08-04 19:00:23 +0200

    implement toString()
    add workaround for Quaternion * Vector3d
    add Vector3d
    add convenient Similar method
    add IEquatable interface
    update README

---
+++ Editor/VectordDrawer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(Vector2d))]
+[CustomPropertyDrawer(typeof(Vector3d))]
+public class VectordDrawer : PropertyDrawer
+{
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+	{
+		EditorGUI.BeginProperty(position, label, property);
+
+		position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Keyboard), label);
+		int indent = EditorGUI.indentLevel;
+		EditorGUI.indentLevel = 0;
+
+		float labelWidth = EditorGUIUtility.singleLineHeight-3;
+		float inputWidth = Mathf.Floor(position.width / 3 - labelWidth);
+		var labelX = new Rect(position.x-1, position.y, labelWidth, position.height);
+		var rectX = new Rect(labelX.xMax, position.y, inputWidth, position.height);
+		var labelY = new Rect(rectX.xMax + 2, position.y, labelWidth, position.height);
+		var rectY = new Rect(labelY.xMax, position.y, inputWidth, position.height);
+
+		EditorGUI.LabelField(labelX, "X");
+		EditorGUI.LabelField(labelY, "Y");
+		EditorGUI.PropertyField(rectX, property.FindPropertyRelative("x"), GUIContent.none);
+		EditorGUI.PropertyField(rectY, property.FindPropertyRelative("y"), GUIContent.none);
+		SerializedProperty z = property.FindPropertyRelative("z");
+		if (z != null)
+		{
+			var labelZ = new Rect(rectY.xMax + 2, position.y, labelWidth, position.height);
+			var rectZ = new Rect(labelZ.xMax, position.y, inputWidth, position.height);
+			EditorGUI.LabelField(labelZ, "Z");
+			EditorGUI.PropertyField(rectZ, z, GUIContent.none);
+		}
+
+		EditorGUI.indentLevel = indent;
+		EditorGUI.EndProperty();
+	}
+}
--- README.md
+++ README.md
@@ -2,3 +2,15 @@ vector3d
 ========
 
 Double precision Vector Libraries for Unity3D
+
+### Non Standard Methods
+
+#### Vector2d.Similar / Vector3d.Similar
+`public bool Similar(Vector3d other, double epsilon = EPSILON)`
+
+Tests the vector componentwise for similarity.
+
+#### Vector3d.Rotated
+`public Vector3d Rotated(Quaternion rotation)`
+
+Workaround for the construct: `Quaternion * Vector3d`
